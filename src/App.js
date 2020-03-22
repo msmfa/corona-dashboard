b5badea1007cd4661482b7b026d9caae56afbde5
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-22 17:50:43 +0000

    Added Grid system

--- src/App.js
+++ src/App.js
@@ -25,9 +25,48 @@ class App extends Component {
         <h2>confirmed: {this.state.confirmed}</h2>
         <h2>recovered: {this.state.recovered}</h2>
         <h2>deaths: {this.state.deaths}</h2>
+
+        <Display />
       </div>
     );
   }
 }
+
+class Display extends Component {
+  createTable = () => {
+    let div = [];
+
+    for (let i = 0; i < 28; i++) {
+      let children = [];
+
+      for (let j = 0; j < 28; j++) {
+        children.push(<div className="box"></div>);
+      }
+
+      div.push(<div className="box">{children}</div>);
+    }
+    return div;
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="box-container">{this.createTable()}</div>
+      </div>
+    );
+  }
+}
+
+// function createDivs() {
+//   for (let i = 0; i < 17 * 17; i++) {
+//     const content = document.createElement("div");
+//     content.classList.add("content");
+//     container.appendChild(content);
+//     const box = document.querySelectorAll("div.content");
+//     box.forEach(box => {
+//       box.addEventListener("mouseover", changeColour);
+//     });
+//   }
+// }
 
 export default App;
