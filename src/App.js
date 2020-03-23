7b20d9a8feba59328748cd196dbbf4bd9129d870
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-23 13:06:09 +0000

    Updated Componants

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LogoSick from "./IMG/human-sick.PNG";
 import LogoSickTwo from "./IMG/human-icon-sick2.png";
 import LogoHealthy from "./IMG/human-icon-healthy.png";
 import LogoGrey from "./IMG/defaulthuman.png";
+
 class App extends Component {
   state = {
     confirmed: 0
@@ -23,78 +24,134 @@ class App extends Component {
 
   render() {
     return (
-      <div className="main-container">
-        <div className="display-left">
-          <h1 className="title">Visualising COVID-19</h1>
-          <div className="numbers-container">
-            <h3 className="numbers">{this.state.confirmed}</h3>
-            <h3 className="numbers">{this.state.recovered}</h3>
-            <h3 className="numbers">{this.state.deaths}</h3>
-            <h3 className="subtitle">confirmed </h3>
-            <h3 className="subtitle">recovered </h3>
-            <h3 className="subtitle">deaths</h3>
-          </div>
-          <div className="grey-line"></div>
-          <p>
-            This Website helps to visualise the impact of COVID-19 and it's
-            spread.
-          </p>
-          <div className="image-container">
-            <img className="image-amount" src={LogoGrey} alt="human"></img>
-            <p className="image-text"> = 1000 People</p>
-          </div>
-          <p>
-            The vast numbers infected can make it hard to visualise the
-            relationship between those infected, those who recover and those who
-            die from the virus. This website is an attempt to solve that
-            problem.
-          </p>
-          <p className="text-lower">
-            The data displayed is from the{" "}
-            <a
-              className="who-link"
-              href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              WHO
-            </a>{" "}
-            and is updated every minute
-          </p>
-          <div className="grey-line"></div>
-          <p className="display-left-text">
-            <div className="image-container-main">
-              <div className="image-container">
-                <img className="recIcon" src={LogoSickTwo} alt="human"></img>
-                <p> = Confirmed Cases</p>
-              </div>
-              <div className="image-container">
-                <img className="recIcon" src={LogoHealthy} alt="human"></img>
-                <p> = Recovered</p>
-              </div>
-              <div className="image-container">
-                <img className="recIcon" src={LogoSick} alt="human"></img>
-                <p> = Deaths</p>
-              </div>
-            </div>
-          </p>
-          <div className="credit">
-            Data API provided by{" "}
-            <a
-              className="link"
-              href="https://github.com/mathdroid"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Mathdroid.
-            </a>
-          </div>
-        </div>
+      <div className="flex-container">
+        <LeftDisplay
+          confirmed={this.state.confirmed}
+          recovered={this.state.recovered}
+          deaths={this.state.deaths}
+        />
         <DisplayDeaths
           deaths={this.state.deaths}
           recovered={this.state.recovered}
           confirmed={this.state.confirmed}
         />
+      </div>
+    );
+  }
+}
+
+class LeftDisplay extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="display-left">
+          <Header />
+          <DisplayNumbers
+            deaths={this.props.deaths}
+            recovered={this.props.recovered}
+            confirmed={this.props.confirmed}
+          />
+          <DisplayText />
+          <DisplayIcons />
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
+
+class DisplayIcons extends Component {
+  render() {
+    return (
+      <p className="display-left-text">
+        <div className="image-container-main">
+          <div className="image-container">
+            <img className="recIcon" src={LogoSickTwo} alt="human"></img>
+            <p> = Confirmed Cases</p>
+          </div>
+          <div className="image-container">
+            <img className="recIcon" src={LogoHealthy} alt="human"></img>
+            <p> = Recovered</p>
+          </div>
+          <div className="image-container">
+            <img className="recIcon" src={LogoSick} alt="human"></img>
+            <p> = Deaths</p>
+          </div>
+        </div>
+      </p>
+    );
+  }
+}
+
+class DisplayText extends Component {
+  render() {
+    return (
+      <>
+        <div className="grey-line"></div>
+        <p>
+          This Website helps to visualise the impact of COVID-19 and it's
+          spread.
+        </p>
+        <div className="image-container">
+          <img className="image-amount" src={LogoGrey} alt="human"></img>
+          <p className="image-text"> = 1000 People</p>
+        </div>
+        <p>
+          The vast numbers infected can make it hard to visualise the
+          relationship between those infected, those who recover and those who
+          die from the virus. This website is an attempt to solve that problem.
+        </p>
+        <p className="text-lower">
+          The data displayed is from the{" "}
+          <a
+            className="who-link"
+            href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            WHO
+          </a>{" "}
+          and is updated every minute
+        </p>
+        <div className="grey-line"></div>
+      </>
+    );
+  }
+}
+class Header extends Component {
+  render() {
+    return <h1 className="title">Visualising COVID-19</h1>;
+  }
+}
+
+class DisplayNumbers extends Component {
+  render() {
+    return (
+      <div className="numbers-container">
+        <h3 className="numbers">{this.props.confirmed}</h3>
+        <h3 className="numbers">{this.props.recovered}</h3>
+        <h3 className="numbers">{this.props.deaths}</h3>
+        <h3 className="subtitle">confirmed </h3>
+        <h3 className="subtitle">recovered </h3>
+        <h3 className="subtitle">deaths</h3>
+      </div>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="credit">
+        Data API provided by{" "}
+        <a
+          className="link"
+          href="https://github.com/mathdroid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mathdroid.
+        </a>
       </div>
     );
   }
