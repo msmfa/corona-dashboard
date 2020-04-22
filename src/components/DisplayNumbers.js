b5a64212874d1c3412a8314f86e94aec1b0fcef8
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-04-22 16:51:29 +0100

    Added commas to numbers

--- src/components/DisplayNumbers.js
+++ src/components/DisplayNumbers.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 
 class DisplayNumbers extends Component {
   render() {
+    const confirmed = [this.props.confirmed].toLocaleString();
+    const recovered = [this.props.recovered].toLocaleString();
+    const deaths = [this.props.deaths].toLocaleString();
+
     return (
       <div className="numbers-container">
-        <h3 className="numbers">{this.props.confirmed}</h3>
-        <h3 className="numbers">{this.props.recovered}</h3>
-        <h3 className="numbers">{this.props.deaths}</h3>
+        <h3 className="numbers">{confirmed}</h3>
+        <h3 className="numbers">{recovered}</h3>
+        <h3 className="numbers">{deaths}</h3>
         <h3 className="subtitle">confirmed </h3>
         <h3 className="subtitle">recovered </h3>
         <h3 className="subtitle">deaths</h3>
